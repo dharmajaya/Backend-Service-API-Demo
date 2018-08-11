a57089d42248a3a991ef1fc1183297a2dfc38d0c
Raja <>
2018-08-11 19:59:10 +0530

    Demo API raja

---
+++ controllers/user_encryption.go
@@ -0,0 +1,14 @@
+package controllers
+
+import "../vendor/golang.org/x/crypto/bcrypt"
+
+func clearMemory(b []byte) {
+  for i := 0; i < len(b); i++ {
+    b[i] = 0;
+  }
+}
+
+func CryptPassword(password []byte) ([]byte, error) {
+    defer clearMemory(password)
+    return bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
+}
